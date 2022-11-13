eb2615d506118d491a81d46f35dc5d0e1fdb9bad
listopadgeorgii <listopadiogl@gmail.com>
2022-11-13 19:48:30 +0300

    let,const,var,prompt,alert

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
 // Переменные
 // camelCase
 const firstName = 'Georgy'
-const lastName = 'Listopad' // string
-let age = 25; // number
+// const lastName = 'Listopad' // string
+const age = 25; // number
 const isProgrammer = true // boolean
+
+const  $ = 'private'
+const _ = 'some value'
+
+// const if = 'error' // err
+const withNum5 = '5'
+ // const 5withNum5 = 5 // er
+
+//2 Мутирование
+
+ // console.log('Имя человека: ' + firstName + ', а возраст человека ' + age)
+ // alert('Имя человека: ' + firstName + ', а возраст человека ' + age)
+
+const lastName = prompt('Введите фамилию')
+alert(firstName + ' ' + lastName)
 
