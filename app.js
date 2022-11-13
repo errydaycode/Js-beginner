1db34654d82eeb9c0b4aa8febf81a2d953de424a
listopadgeorgii <listopadiogl@gmail.com>
2022-11-13 20:31:32 +0300

    data types

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-// Переменные
+// 1 Переменные
 // camelCase
-const firstName = 'Georgy'
+//const firstName = 'Georgy'
 // const lastName = 'Listopad' // string
-const age = 25; // number
-const isProgrammer = true // boolean
+// const age = 25; // number
+//const isProgrammer = true // boolean
 
-const  $ = 'private'
-const _ = 'some value'
+//const  $ = 'private'
+//const _ = 'some value'
 
 // const if = 'error' // err
-const withNum5 = '5'
+//const withNum5 = '5'
  // const 5withNum5 = 5 // er
 
 //2 Мутирование
@@ -17,6 +17,48 @@ const withNum5 = '5'
  // console.log('Имя человека: ' + firstName + ', а возраст человека ' + age)
  // alert('Имя человека: ' + firstName + ', а возраст человека ' + age)
 
-const lastName = prompt('Введите фамилию')
-alert(firstName + ' ' + lastName)
+// const lastName = prompt('Введите фамилию')
+// alert(firstName + ' ' + lastName)
+
+// 3 Операторы
+//let currentYear = 2022
+//const birthYear = 1997
+
+// const age = currentYear + birthYear
+//const a = 10
+//const b = 5
+
+//let c = 32
+
+//c = c + a
+//c = c - a
+//c = c * a
+//c = c / a
+//c +=a
+//c -=a
+//c *=a
+// c /=a
+
+//console.log( a + b )
+//console.log( a - b )
+//console.log( a * b )
+//console.log( a / b )
+
+//console.log(++currentYear)
+//console.log(--currentYear)
+//console.log(c)
+
+// 4 Типы данных
+//const lastName = 'Listopad'
+//const age = 25;
+//const isProgrammer = true
+//let x
+//console.log(typeof isProgrammer)
+//console.log(typeof age)
+//console.log(typeof lastName)
+//console.log(x)
+// console.log(null)
+
+
+
 
