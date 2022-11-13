77e41962c90f06a0b6333f0bcbd522e67a443d44
listopadgeorgii <listopadiogl@gmail.com>
2022-11-14 02:09:55 +0300

    strings

---
+++ app.js
@@ -0,0 +1,33 @@
+//const name = 'Георгий'
+
+//const age = 25
+//function getAge () {
+  //  return age
+// }
+
+// const output = 'Привет, меня зовут ' + name + ' и мой возраст ' + age + 'лет.'
+//const output = `Привет, меня зовут ${name} и мой возраст ${age < 20 ? 'A' : 'B'} лет.`
+
+// console.log(output)
+
+// const output = ` <div>This is div </div>
+ //<p>This is p</p> `
+// console.log(output)
+
+const name = 'Георгий Леонидыч'
+/*
+console.log(name.length)
+console.log(name.toUpperCase())
+console.log(name.toLowerCase())
+console.log(name.charAt(2))
+console.log(name.indexOf('нид'))
+console.log(name.toLowerCase().startsWith('гео'))
+console.log(name.startsWith('Гео'))
+console.log(name.endsWith('ыч'))
+console.log(name.repeat(2))
+const string = '    password   '
+console.log(string.trim())
+console.log(string.trimLeft())
+
+*/
+
