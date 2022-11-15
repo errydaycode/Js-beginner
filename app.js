198ef7406eaf2ba0d55a6768d7575753709ddcfa
listopadgeorgii <listopadiogl@gmail.com>
2022-11-15 15:20:03 +0300

    setnterval||timeout

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
 // 1 Функции
-// Function Declaration
-function greet(name) {
-    console.log('Привет - ', name)
-}
 
+// Function Declaration
+//function greet(name) {
+   // console.log('Привет - ', name)
+//}
 
 //Function expression
-const greet2 = function greet2 (name) {
-    console.log('Привет2 - ', name)
-}
+//const greet2 = function greet2 (name) {
+   // console.log('Привет2 - ', name)
+//}
 
-greet('Аня')
-greet2('Аня')
+
+//greet('Аня')
+//greet2('Аня')
+
+//console.log(typeof greet)
+
+//console.dir(greet) Функкции - объекты
+
+// Анонимные функции
+let counter = 0
+const interval = setInterval(function (){
+    if (counter === 5) {
+        clearInterval(interval)
+    } else {
+        console.log(++counter)
+    }
+}, 1000)
 
